bc2a7501768a7179f978346e83d9c370b5f771cc
JasonSnow123 <m.jason.2018@gmail.com>
2023-02-21 17:49:05 -0500

    removes unneeded code

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,7 +3,7 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
 
-  const { days, day, setDay } = props 
+  const { days, setDay } = props 
 
   const dayListMap = days.map((day) => {
     return <DayListItem key={day.id} selected={day.name === props.day} {...day} setDay={() => setDay(day.name)}/>
