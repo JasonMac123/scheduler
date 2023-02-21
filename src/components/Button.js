00694d49f6a292d0aa186f268ff964a01c8fb42f
JasonSnow123 <m.jason.2018@gmail.com>
2023-02-21 16:41:03 -0500

    adds button with different apperances and tests for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,12 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  const { children, confirm, danger, onClick, disabled } = props
+
+   const classes = classNames('button ', {'button--danger': danger}, {'button--confirm': confirm})
+
+  return <button onClick={onClick} className={classes} disabled={disabled}>{children}</button>;
 }
