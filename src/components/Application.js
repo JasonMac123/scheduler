ffa3af5baa64a3bb653824b504a25f44ba1bfca4
JasonSnow123 <m.jason.2018@gmail.com>
2023-02-22 18:01:46 -0500

    adds appointments with constant data

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
-
+import "components/Appointment/index";
+import Appointment from "components/Appointment/index";
 export default function Application(props) {
 
   const [ day, setDay ] = useState('Monday')
@@ -23,6 +24,48 @@ export default function Application(props) {
       spots: 0,
     },
   ];
+  const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
+  const appointmentList = Object.values(appointments).map((appointment) => {
+    return <Appointment key={appointment.id} {...appointment} />
+  }) 
 
   return (
     <main className="layout">
@@ -48,7 +91,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentList}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -170,3 +171,22 @@ storiesOf("Appointment", module)
   .add("Error" , () => (
     <Error message={"Could not delete appointment"} onClose={action("onClose")} />
   ))
+  .add("Form" , () => (
+    <Form student={"Jason"} interviewer={3} interviewers={interviewers} onCancel={action("onCancel")} onSave={action("onSave")} />
+  ))
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+.add("Appointment Booked", () => (
+  <Fragment>
+    <Appointment
+      id={1}
+      time="4pm"
+      interview={{ student: "Lydia Miller-Jones", interviewer }}
+    />
+    <Appointment time="5pm" />
+  </Fragment>
+))
