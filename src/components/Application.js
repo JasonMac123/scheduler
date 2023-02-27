767a2d4135b1d6cc459ea4e6c4f2dddef5cd8033
JasonSnow123 <m.jason.2018@gmail.com>
2023-02-27 15:12:26 -0500

    Refactored state to custom hook and moved to different file

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import React from "react";
 
 import "components/Application.scss";
 import "components/Appointment/index";
 
+import useApplicationData from "hooks/useApplicationData";
 import DayList from "./DayList";
 import Appointment from "components/Appointment/index";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
@@ -11,51 +11,7 @@ import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "help
 
 export default function Application(props) {
 
-  const [state, setState ] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  })
-
-  const setDay = day => setState({ ...state, day });
-
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then((all) => {
-      setState((prev) => ({...prev, days:all[0].data, appointments:all[1].data, interviewers:all[2].data}))
-    })
-  }, [])
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-
-    if (interview === null) {
-      appointment.interview = null;
-    }
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    const data = {...appointment}
-
-    if (appointment.interview === null) {
-      return axios.delete(`/api/appointments/${id}`)
-        .then(() => setState({...state, appointments}))
-        .catch((e) => console.log(e))
-    } 
-    return axios.put(`/api/appointments/${id}`, data)
-      .then(() => setState({...state, appointments}))
-      .catch((e) => console.log(e))
-  }
+  const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
 
   let dailyAppointments = getAppointmentsForDay(state, state.day);
 
@@ -69,6 +25,7 @@ export default function Application(props) {
       interview={interview}
       interviewers={interviewerArray}
       bookInterview={bookInterview}
+      cancelInterview={cancelInterview}
     />
   }) 
 
